a0e3490717441bce37fdac5fb931c72d8d46bcc6
Clive Tsungu <ctsungu@gmail.com>
2022-02-23 13:25:20 +0000

    Mysql Database setup

--- serverless/config/next.config.js
+++ serverless/config/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    'MYSQL_HOST': '127.0.0.1',
+    'MYSQL_PORT': '3306',
+    'MYSQL_DATABASE': {database_name},
+    'MYSQL_USER': {user_name},
+    'MYSQL_PASSWORD': {user_password},
+  }
+}
