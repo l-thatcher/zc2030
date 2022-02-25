ada4d92cbb6826796f87e6bd959b07212bb45685
c1919822 <rowbothamt@cardiff.ac.uk>
2022-02-25 11:37:38 +0000

    feature: created connection string using sequelize

--- serverless/utils/db.js
+++ serverless/utils/db.js
@@ -1,15 +1,32 @@
-import mysql from 'serverless-mysql';
-const db = mysql({
-    config: {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        database: process.env.MYSQL_DATABASE,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    }
+import {Sequelize} from "sequelize";
+import dotenv from 'dotenv';
+import * as fs from "fs";
+
+const { Sequelize } = require('@sequelize/core');
+
+dotenv.config();
+
+
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    logging: false,
+    dialect: process.env.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    dialectOptions: {
+        encrypt: true
+    },
 });
 
-export default async function excuteQuery({ query, values }) {
+export default async function executeQuery({ query, values }) {
     try {
         const results = await db.query(query, values);
         await db.end();
