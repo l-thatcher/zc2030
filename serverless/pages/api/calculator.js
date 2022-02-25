91c27d309a51b9f06852bb4d38de41d464fcea30
c1847017 <yiallouridess@cardiff.ac.uk>
2022-02-25 16:33:16 +0000

    Add Calculator API endpoint

--- serverless/pages/api/calculator.js
+++ serverless/pages/api/calculator.js
@@ -1,5 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {sql_query} from "../../utils/db";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const getCalculatorCategory = `SELECT * FROM CALC_CATEGORY`;
+const getCalculatorInput = `SELECT * FROM CALC_INPUT`;
+const getCalculatorResult = `SELECT * FROM CALC_INPUT`;
+
+
+const handler = async (_, res) => {
+    try {
+        const result = await sql_query( getCalculatorCategory
+        );
+
+        return res.json(result)
+    } catch (e) {
+
+        console.log(res)
+        res.status(500).json({ message: e.message});
+
+    }
+};
+
+
+
+export default handler;
