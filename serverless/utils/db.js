2d7d26ebcf89a24e91543f3faa7c90ff568a7e71
c1919822 <rowbothamt@cardiff.ac.uk>
2022-02-26 15:37:03 +0000

    [29] Feature: Working towards adding next auth Co-authored-by: Clive Tsungu <tsunguc@cardiff.ac.uk>

--- serverless/utils/db.js
+++ serverless/utils/db.js
@@ -4,28 +4,32 @@ import UserModel from '../models/UserModel';
 const { Sequelize } = require('@sequelize/core');
 
 dotenv.config();
-
 export const sequelizeConfig = new Sequelize(
   process.env.DB,
   process.env.USER,
   process.env.PASSWORD,
   {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    logging: false,
-    dialect: process.env.dialect,
-    operatorsAliases: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    dialectOptions: {
-      encrypt: true,
-    },
+      host: process.env.HOST,
+      port: process.env.PORT,
+      logging: false,
+      dialect: process.env.DIALECT,
+      operatorsAliases: false,
+      pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+      },
+      dialectOptions: {
+          ssl: {
+              ca: "../certificates/DigiCertGlobalRootCA.crt.pem",
+              rejectUnauthorized: false,
+              require: true
+          }
+      },
   }
 );
+
 
 const db = {};
 
