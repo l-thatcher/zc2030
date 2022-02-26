d4e17740b1d21eaa97f3fdf6c7db4ca1e0d9be50
Clive Tsungu <ctsungu@gmail.com>
2022-02-26 13:02:13 +0000

    [29] refactor: changed db setup Co-authored-by: Tom Rowbotham <RowbothamT@cardiff.ac.uk>

--- serverless/utils/db.js
+++ serverless/utils/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import * as fs from 'fs';
-
+import UserModel from '../models/UserModel';
 const { Sequelize } = require('@sequelize/core');
 
 dotenv.config();
@@ -29,18 +29,23 @@ export const sequelizeConfig = new Sequelize(
   }
 );
 
-/**
- *
- * @param {*} param0
- * @returns
- */
-
 try {
   await sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelizeConfig = sequelizeConfig;
+
+db.UserModel = UserModel;
+
+db.sequelizeConfig.sync({ force: false }).then(() => {
+  console.log('yes re-sync done!');
+});
 
 export default async function executeQuery({ query, values }) {
   try {
