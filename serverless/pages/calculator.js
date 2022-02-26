9214023b858f1075c62d256038626796d9eb76de
c1847017 <yiallouridess@cardiff.ac.uk>
2022-02-26 20:16:12 +0000

    [31] Feature: Update calculator page by creating a separate calculator component

--- serverless/pages/calculator.js
+++ serverless/pages/calculator.js
@@ -1,60 +1,42 @@
-import styles from '../styles/Calculator.module.css'
-import {
-  Form, Button
-} from 'react-bootstrap';
+import CarbonCalculator from "../Components/CarbonCalculator"
+import styles from "../styles/Calculator.module.css"
 
 export default function Calculator() {
+
+    // Using json data object for TESTING - This is going to be removed
+    const jsonData = {
+        Category1: [
+            { id: '1', name:`Individual`, category: ['Food', `Transport`, `Energy`], results: [14, 56, 89] }
+        ],
+        Category2: [
+            { id: '2', name:`Business`, category: ['Zoo', `Jungle`, `Forest`], results: [1, 98, 32]  }
+        ],
+        Category3: [
+            { id: '3', name:`Food`, category: ['Football', `Rugby`, `Basketball`], results: [10, 100, 33]  }
+        ],
+        Category4: [
+            { id: '4', name:`Drinks`, category: ['Water', `Vodka`, `Sprite`], results: [95, 100, 100]  }
+        ],
+        Category5: [
+            { id: '5', name:`University`, category: ['Books', `Pencil`, `Rubber`], results: [5, 89, 10]  }
+        ]
+    };
+
+    // Data with key values
+    const data =  Object.keys(jsonData).reduce((accumulator, iterator) => {
+        return [...accumulator, ...jsonData[iterator]];
+    }, [])
+
   return (
-      <main className={styles.main}>
-        <div className={styles.titleDiv}>
-          <h1>
-            Carbon Calculator
-          </h1>
-        </div>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Carbon Calculators</h1>
 
-        <div className={styles.pageContent}>
-            <div className={styles.calculatorBody}>
-                <h3>
-                    Select a category:
-                </h3>
-                <div className={styles.formDiv}>
-                    <Form.Check
-                        className={styles.form}
-                        type="radio"
-                        id={`custom-radio item-`}
-                        name="category"
-                        label={"Dummy"}
-                        value={"Dummy"}
-                    />
-                    <Form.Check
-                        className={styles.form}
-                        type="radio"
-                        id={`custom-radio item-`}
-                        name="category"
-                        label={"Dummy"}
-                        value={"Dummy"}
-                    />
-                    <Form.Check
-                        className={styles.form}
-                        type="radio"
-                        id={`custom-radio item-`}
-                        name="category"
-                        label={"Dummy"}
-                        value={"Dummy"}
-                    />
-                </div>
-                <div>
-                    <Button variant="primary" size="lg" type="submit" className={styles.button}>
-                        Next
-                    </Button>
-                </div>
-            </div>
-            <div className={styles.progressBar}>
-                <h3>progress component</h3>
-            </div>
-        </div>
+          {
+              data.map((type, i) =>
+                  <CarbonCalculator data={data[i].name} id={i} category={data[i].category} results={data[i].results}/>
+              )
+          }
 
-
-      </main>
+      </div>
   )
 }
