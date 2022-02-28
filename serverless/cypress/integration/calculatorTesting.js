7e07b0bbca148e06cdd8fef9fb9531dd0f7ce01b
c1847017 <yiallouridess@cardiff.ac.uk>
2022-02-28 15:39:36 +0000

    [31] Test: Add more tests on calculator and progress bar using Cypress

--- serverless/cypress/integration/calculatorTesting.js
+++ serverless/cypress/integration/calculatorTesting.js
@@ -14,3 +14,47 @@ describe('Navigation', () => {
         cy.get('h1').contains('Carbon Calculator')
     })
 })
+
+describe('Calculator Headings', () => {
+    it('should find calculator and category headings', () => {
+
+        // Visits page
+        cy.visit('/calculator')
+
+        // Checks calculator heading
+        cy.get('h2').contains('Calculator')
+
+        // Checks category heading
+        cy.get('h3').contains('Select a category:')
+
+        // Checks category headings
+        cy.get('label.form-check-label').contains('Food')
+        cy.get('label.form-check-label').contains('Transport')
+        cy.get('label.form-check-label').contains('Energy')
+
+    })
+})
+
+describe('Progress Bar Elements', () => {
+    it('should find progress bar and category headings, and percentages', () => {
+
+        // Visits page
+        cy.visit('/calculator')
+
+        // Checks Progress Bar heading
+        cy.get('h3').contains('Progress Bar')
+
+        // Checks category name and percentage
+        cy.get('h1').contains('Food')
+        cy.get('text.CircularProgressbar-text').contains('14%')
+
+        // Checks category name and percentage
+        cy.get('h1').contains('Transport')
+        cy.get('text.CircularProgressbar-text').contains('56%')
+
+        // Checks category name and percentage
+        cy.get('h1').contains('Energy')
+        cy.get('text.CircularProgressbar-text').contains('89%')
+
+    })
+})
