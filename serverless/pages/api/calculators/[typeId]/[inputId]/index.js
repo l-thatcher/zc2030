580781c44e681d46a5276e833ef1442419aaa0fd
c1847017 <yiallouridess@cardiff.ac.uk>
2022-03-03 23:09:56 +0000

    [63] Feature: Create API calls for Calculators, Calculator type, Calculator Input

---
+++ serverless/pages/api/calculators/[typeId]/[inputId]/index.js
@@ -0,0 +1,36 @@
+import {execute_query} from "../../../../../utils/db";
+
+const getCalculatorInputsByCategoryId = ` SELECT CalculatorInput.id, CalculatorInput.name, CalculatorInput.factor, CalculatorInput.unit 
+                                          FROM CalculatorCategory
+                                          JOIN CalculatorInput ON CalculatorCategory.id = CalculatorInput.category_id
+                                          WHERE CalculatorCategory.id = ?`;
+
+export default async function handler(req, res) {
+
+    const {inputId} = req.query
+    switch (req.method) {
+
+        // Get data from database
+        case 'GET':
+            try {
+                const result = await execute_query(getCalculatorInputsByCategoryId, inputId);
+                res.status(200).json(result);
+
+            } catch (e) {
+                res.status(500).json({message: e.message});
+            }
+            break;
+
+        // Create data from database
+        case 'POST':
+            break;
+
+        // Delete data from database
+        case 'DELETE':
+            break;
+
+        // Update data from database
+        case 'PUT':
+            break;
+    }
+};
