11c226c188817847edb9015bb621de3171152e87
Clive Tsungu <ctsungu@gmail.com>
2022-03-08 18:28:36 +0000

    [25] feature: fixed tailwind and added header for product details Co-authored-by: Nikrad Nassiri <nassirin@cardiff.ac.uk>

--- serverless/pages/projects/[id].js
+++ serverless/pages/projects/[id].js
@@ -1,12 +1,37 @@
+import ProjectDetails from "../../Components/ProjectDetails";
 
 
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
 
-const Details = () =>{
+    const paths = data.map( project=>{
+        return{
+            params: {id: project.id.toString()}
+        }
+    })
+
+    return{
+        paths, fallback: false
+    }
+
+}
+
+export const getStaticProps = async (context) =>{
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id);
+    const data = await res.json();
+
+    return{
+        props: {project: data}
+    }
+}
+
+const Details = ({project}) =>{
 
     return(
-
-        <div>
-            <h1>Details Page</h1>
+        <div  >
+            <ProjectDetails  />
         </div>
     )
 
