c4e6a20d2d26b1c27253ce905dbc7649da7ba45e
c1919822 <rowbothamt@cardiff.ac.uk>
2022-03-10 16:06:02 +0000

    [66] Feature: Added dynamic URL to edit calculator

--- serverless/Components/admin/calculators/CalculatorTypes.js
+++ serverless/Components/admin/calculators/CalculatorTypes.js
@@ -1,4 +1,5 @@
 import {Button, Container, Nav} from "react-bootstrap";
+import Link from 'next/link'
 import styles from "../../../styles/Calculator.module.css";
 
 const CalculatorTypes = (data) => {
@@ -8,7 +9,9 @@ const CalculatorTypes = (data) => {
             <h1>Edit Calculators <Button variant="primary" size="lg">+</Button></h1>
             <div className="d-grid gap-2">
                 {types.map(it =>
+                    <Link href={`/admin/editCalculator/${it.id}`}>
                     <Button variant="primary" size="lg">{it.name}</Button>
+                    </Link>
                 )}
             < /div>
         </Container>
