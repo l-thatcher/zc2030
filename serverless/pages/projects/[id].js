4c06a9c0101134295281e99aaf3fa3d01d362a31
Clive Tsungu <ctsungu@gmail.com>
2022-03-10 00:12:11 +0000

    [25] feature: dynamic page routing for 'projects' Co-authored-by: Nikrad Nassiri <nassirin@cardiff.ac.uk>

--- serverless/pages/projects/[id].js
+++ serverless/pages/projects/[id].js
@@ -30,7 +30,7 @@ export const getStaticProps = async (context) =>{
 const Details = ({project}) =>{
 
     return(
-        <div  >
+        <div className='bg-gray-50' >
             <ProjectDetails  />
         </div>
     )
