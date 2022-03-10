51d016bc33cdaec3f283d35c1e5158cd8781124b
Clive Tsungu <ctsungu@gmail.com>
2022-03-10 00:13:05 +0000

    [25] feature: created dynamic progress bar component Co-authored-by: Nikrad Nassiri <nassirin@cardiff.ac.uk>

---
+++ serverless/Components/ProgressB.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Progress from "@material-tailwind/react/Progress";
+
+function ProgressB(props) {
+
+    const widthValue =props.value
+    return (
+        <div className="w-full bg-gray-200 rounded-full">
+            <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                 style={{width: widthValue}} > {widthValue}
+            </div>
+        </div>
+    );
+}
+
+export default ProgressB;
