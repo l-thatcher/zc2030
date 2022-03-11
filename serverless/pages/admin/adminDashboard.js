2684fa5434fa4305e8bb278642e2a8aaa21458ca
Louis <thatcherlg@cardiff.ac.uk>
2022-03-11 18:46:02 +0000

    [69] FEATURE: Set up back end requests for the admin panel to populate fields.
    Co-authored-by: Sotiris Yiallourides

--- serverless/pages/admin/adminDashboard.js
+++ serverless/pages/admin/adminDashboard.js
@@ -3,70 +3,70 @@ import AdminSidebar from "../../Components/AdminSidebar";
 import {AiOutlineEdit, AiOutlinePlusSquare, AiOutlineSearch} from 'react-icons/Ai';
 import {MdDelete} from "react-icons/Md";
 
-export default function adminDashboard() {
-    const adminJsonData = {
-        Category1: [
-            {
-                id: "1",
-                name: `peter`,
-                email: 'peter@zc30.com',
-            },
-            {
-                id: "2",
-                name: `peter`,
-                email: 'peter@zc30.com',
-            },
-            {
-                id: "3",
-                name: `alfred`,
-                email: 'alfred@zc30.com',
-            },
-            {
-                id: "4",
-                name: `alfred`,
-                email: 'alfred@zc30.com',
-            },
-        ]
-    };
+export default function adminDashboard(props) {
+    const users = props.users;
+    // const adminJsonData = {
+    //     Category1: [
+    //         {
+    //             id: "1",
+    //             name: `peter`,
+    //             email: 'peter@zc30.com',
+    //         },
+    //         {
+    //             id: "2",
+    //             name: `peter`,
+    //             email: 'peter@zc30.com',
+    //         },
+    //         {
+    //             id: "3",
+    //             name: `alfred`,
+    //             email: 'alfred@zc30.com',
+    //         },
+    //         {
+    //             id: "4",
+    //             name: `alfred`,
+    //             email: 'alfred@zc30.com',
+    //         },
+    //     ]
+    // };
+    //
+    // const userJsonData = {
+    //     Category1: [
+    //         {
+    //             id: "1",
+    //             name: `bob`,
+    //             email: 'alfred@zc30.com',
+    //             type: 'personal',
+    //         },
+    //         {
+    //             id: "2",
+    //             name: `tod`,
+    //             email: 'alfred@zc30.com',
+    //             type: 'business',
+    //         },
+    //         {
+    //             id: "3",
+    //             name: `hob`,
+    //             email: 'alfred@zc30.com',
+    //             type: 'business',
+    //         },
+    //         {
+    //             id: "4",
+    //             name: `lob`,
+    //             email: 'alfred@zc30.com',
+    //             type: 'school',
+    //         },
+    //
+    //     ]
+    // };
 
-    const userJsonData = {
-        Category1: [
-            {
-                id: "1",
-                name: `bob`,
-                email: 'alfred@zc30.com',
-                type: 'personal',
-            },
-            {
-                id: "2",
-                name: `tod`,
-                email: 'alfred@zc30.com',
-                type: 'business',
-            },
-            {
-                id: "3",
-                name: `hob`,
-                email: 'alfred@zc30.com',
-                type: 'business',
-            },
-            {
-                id: "4",
-                name: `lob`,
-                email: 'alfred@zc30.com',
-                type: 'school',
-            },
-
-        ]
-    };
-
-    const adminData = Object.keys(adminJsonData).reduce((accumulator, iterator) => {
-        return [...accumulator, ...adminJsonData[iterator]];
-    }, []);
-
-    const userData = Object.keys(userJsonData).reduce((accumulator, iterator) => {
-        return [...accumulator, ...userJsonData[iterator]];
-    }, []);
-
+    // const adminData = Object.keys(adminJsonData).reduce((accumulator, iterator) => {
+    //     return [...accumulator, ...adminJsonData[iterator]];
+    // }, []);
+    //
+    // const userData = Object.keys(userJsonData).reduce((accumulator, iterator) => {
+    //     return [...accumulator, ...userJsonData[iterator]];
+    // }, [])
     return (
         <div className={styles.main}>
             <AdminSidebar/>
@@ -100,10 +100,10 @@ export default function adminDashboard() {
                     <div className={styles.lists}>
                         {adminData.map((name, i) => (
                             <div className={styles.items}>
-                                <p>{userData[i].id}</p>
-                                <p>{userData[i].name}</p>
-                                <p>{userData[i].email}</p>
-                                <p>{userData[i].type}</p>
+                                <p>{users[i].id}</p>
+                                <p>{users[i].name}</p>
+                                <p>{users[i].email}</p>
+                                <p>{users[i].type}</p>
                                 <AiOutlineEdit className={styles.icons}/>
                             </div>
                         ))}
