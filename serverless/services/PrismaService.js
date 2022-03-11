d70fc338ddd66788c1fad5741dc94b2b340f9aa6
c1989698 <nassirin@cardiff.ac.uk>
2022-03-11 01:14:14 +0000

    [25] Feature: Project Card list with prelim UI completed including Projects Database table and /api/projects api route: co-authors:Tom Rowbothman <rowbothmant@cardiff.ac.uk> Clive Tsungu <tsunguc@cardiff.ac.uk>

---
+++ serverless/services/PrismaService.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// export default async function FilledResultsByCategory(userId, inputIds) {
+//     const results = await prisma.calculatorResult.findMany({
+//         where: {
+//             user_id: userId,
+//             input_id: {in: inputIds}
+//         },
+//     })
+// }
+
+export const getInputsByCategory = (categoryId) => {
+    return prisma.calculatorInput.findMany({
+        where: {
+            category_id: categoryId,
+        },
+        select: {
+            id: true,
+        },
+    });
+};
+
+export const getCountOfUsersUniqueFilledResultsByCategory = (
+    userId,
+    idArray
+) => {
+    return prisma.calculatorResult.findMany({
+        distinct: ["input_id"],
+        where: {
+            user_id: userId,
+            input_id: { in: idArray },
+        },
+    });
+};
+
+export const getListofProjects = () => {
+    return prisma.projects.findMany();
+};
