8293bdb374784f6d16cfa1fea85baa9027273edb
Clive Tsungu <ctsungu@gmail.com>
2022-03-11 18:55:37 +0000

    [25] feature: displayed  map with dynamic location markers on to the product details page  Co-authored-by: Nikrad Nassiri <nassirin@cardiff.ac.uk>

--- serverless/Components/projectDetails/ProjectDetails.js
+++ serverless/Components/projectDetails/ProjectDetails.js
@@ -5,17 +5,15 @@ import {LocationMarkerIcon,CalendarIcon} from '@heroicons/react/solid'
 // import image3 from '../public/7a5d17d22fbcb9629f6224458481884b.jpeg'
 // import image4 from '../public/Bogdkhan_Uul_Strictly_Protected_Area,_Mongolia_(149199747).jpg'
 // import image5 from '../public/9a08-20140530-everdale.jpg'
-import { Loader } from '@googlemaps/js-api-loader';
+
 import ProjectImageCard from "./ProjectImageCard";
+import AboutProject from "./AboutProject";
 import BuyCard from "./BuyCard";
 import {FiGlobe, FiFacebook} from "react-icons/fi"
 import Map from './Map'
 
 
 function ProjectDetails(props) {
-
-
-
 
     // const images2 = [image1,image2,image3,image4,image5]
 
@@ -42,10 +40,10 @@ function ProjectDetails(props) {
                         <h1 className="text-[42px] font-semibold leading-7 text-gray-900 sm:text-3xl sm:truncate">Elveden farm {props.name}</h1>
 
                         <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-                            <div className="mt-2 flex items-center text-sm text-gray-500 border-b border-[#F2F2F2] capitalize">
+                            <div className="mt-2 flex items-center text-sm text-gray-500 underline border-b mb-2 border-[#F2F2F2] capitalize">
                                 {/*location*/}
                                 <LocationMarkerIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-red-400" aria-hidden="true" />
-                                Elveden Estate {props.location} | Norfolk, United Kingdom {props.location}
+                                Elveden Estate {props.location} | Norfolk, United Kingdom {props.location} {props.location}
                             </div>
 
 
@@ -66,8 +64,8 @@ function ProjectDetails(props) {
                                     </ul>
                                 </div>
 
-                            <div className="hidden md:block">
-                                <div className="grid-cols-3  pr-20 pb-10 space-y-2   md:space-y-0 md:grid md:gap-3 md:grid-rows-3 ">
+                            <div className="hidden md:block  ">
+                                <div className="grid-cols-4 space-y-2  md:space-y-0 md:grid md:gap-3  ">
                                     <div className="w-full  col-span-2 row-span-2 rounded ">
                                         <img className='rounded-xl hover:scale-105 transition duration-200 ease-in-out'
                                             src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
@@ -93,11 +91,6 @@ function ProjectDetails(props) {
                                             src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
                                             alt="image"/>
                                     </div>
-                                    <div className="w-full rounded ">
-                                        <img className ='rounded-xl hover:scale-105 transition duration-200 ease-in-out'
-                                             src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                                             alt="image"/>
-                                    </div>
                                 </div>
                             </div>
 
@@ -106,12 +99,8 @@ function ProjectDetails(props) {
 
                         {/*ABOUT Project*/}
                         <div className='mt-4 border-t border-[#F2F2F2] p-4'>
-
                             {/*BUY Carbon*/}
-                            <div className="mt-1">
-                                <div className='flex items-center bg-blue-500'>
-
-                                </div>
+                            <div className="mt-1 md:hidden">
                                 <div className=''>
                                     <h2 className="font-bold  mb-1 text-[34px] text-green-600 pb-3 ">
                                         £30 {props.price}
@@ -119,7 +108,7 @@ function ProjectDetails(props) {
                                          </span>
                                     </h2>
                                     <p className='font-semibold text-gray-400'> Total Supply: {props.totalsupply} </p>
-                                    <p className='font-semibold text-gray-400' > Available Supply: </p>
+                                    <p className='font-semibold text-gray-400' > Available Supply: {props.avlbsupply} </p>
                                     <p className='font-bold'>How many tonnes of carbon would you like to buy?</p>
                                     <input className='w-full form-control border border-solid border-gray-300 rounded block px-6 py-2.5 mb-3' type="number" placeholder='/tCo2e'/>
                                     <button type="button"
@@ -134,77 +123,17 @@ function ProjectDetails(props) {
 
 
 
-
                             {/*Header*/}
-                            <div>
-                                <h2 className='flex justify-between items-center py-3 w-full font-medium text-left text-gray-900 rounded-t-xl border-b border-gray-200 dark:border-gray-700 dark:text-white'>
-                                    <span>About This Project</span>
-                                </h2>
-                            </div>
-
-                            <div className='flex  items-center'>
-
-                                {/*//profile picture*/}
-                                <div className=''>
-                                    <img
-                                        src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
-
-                                        className="rounded-full h-16 shadow-lg"
-                                        alt="Avatar"
-                                    />
+                            <div className='flex'>
+                                <div>
+                                    <AboutProject/>
                                 </div>
-
-                                {/*//name*/}
-                                <div className=' mt-6 px-2'>
-                                    <div>
-                                        <h6 className=" capitalize">John Doe {props.creator}</h6>
-                                    </div>
-                                    <div>
-                                        <p className="text-gray-500 capitalize">farm Owner {props.type}</p>
-                                    </div>
-
-                                </div>
-
-                                {/*//socials*/}
-                                <div className='flex px-10'>
-                                    <span className="text-xl font-bold block uppercase tracking-wide text-gray-900  ">
-                                                <FiGlobe/>
-                                    </span>
-
-                                    <span className="text-xl font-bold block uppercase tracking-wide text-gray-900 ml-4">
-                                                    <FiFacebook/>
-                                    </span>
+                                <div className="hidden md:block ">
+                                    <BuyCard />
                                 </div>
 
                             </div>
 
-                            <div className=''>
-                                {/*Date*/}
-                                <div className="mt-2 flex items-center text-sm text-gray-500">
-                                    <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                    January 9, 2020 {props.date}
-                                </div>
-                                <div className='border-b border-[#F2F2F2] w-20 pt-2 g'>
-
-                                </div>
-                            </div>
-
-                            {/*Description*/}
-
-                            <div className='mt-6 '>
-                                <p className="text-gray-500 line-clamp-4 ">laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
-                                    Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes,
-                                    nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci,
-                                    sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
-                                    Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-                                    Cum sociis natoque penatibus et magnis dis parturient {props.description}</p>
-                            </div>
-
-                            {/*modal*/}
-                            <div className=' border-b border-gray-200'>
-                                <p className="text-gray-500 line-clamp-4 underline font-bold text-green-600 "> Show more > </p>
-                            </div>
 
 
                             {/*Where are we based?*/}
@@ -212,7 +141,11 @@ function ProjectDetails(props) {
                                 <h3 className='flex justify-between items-center py-3 w-full font-normal text-left text-gray-900 rounded-t-xl  '>
                                     <span>Where are we based ?</span>
                                 </h3>
-
+                                <div className="mt-2 flex items-center text-sm text-gray-500 border-b border-[#F2F2F2] capitalize">
+                                    {/*location*/}
+                                    <LocationMarkerIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-red-400" aria-hidden="true" />
+                                    Elveden Estate {props.location} | Norfolk, United Kingdom {props.location} {props.location}
+                                </div>
                                 <Map location={location} />
 
                                 <div>
