04d1bad6258ed474bfd6055727d87ebb8cc2c0ae
c1919822 <rowbothamt@cardiff.ac.uk>
2022-03-12 19:24:06 +0000

    [66] Feature: Added are you sure you want to delete on calculator type

--- serverless/Components/admin/calculators/CalculatorTypes.js
+++ serverless/Components/admin/calculators/CalculatorTypes.js
@@ -1,17 +1,26 @@
-import {Button, Container, Nav} from "react-bootstrap";
+import {Button, Container, Modal, Nav} from "react-bootstrap";
 import Link from "next/link";
 import styles from "../../../styles/Calculator.module.css";
 import {AiFillDelete} from "react-icons/ai";
 import {deleteCalculatorType} from "../../../services/CalculatorService";
 import {useRouter} from "next/router";
+import {useState} from "react";
 
 const CalculatorTypes = (data) => {
     const types = data.types;
     const router = useRouter()
+    const [showModal, setShowModal] = useState(false);
+    const handleClose = () => setShowModal(false);
+    const [idToDelete, setIdToDelete] = useState(null)
 
-    async function handleDelete(idToDelete) {
+    async function handleDelete() {
         await deleteCalculatorType(idToDelete)
         router.reload()
+    }
+
+    function handleOpen(id) {
+        setIdToDelete(id)
+        setShowModal(true)
     }
 
     return (
@@ -33,14 +42,35 @@ const CalculatorTypes = (data) => {
                         <Button
                             variant="danger"
                             id="button-addon2"
-                            onClick={(e) => handleDelete(it.id)}
-                        >                  <AiFillDelete size={20} />
+                            onClick={(e) => handleOpen(it.id)}
+                        > <AiFillDelete size={20}/>
                         </Button>
                     </div>
-                    ))}
-                    </div>
-                    </Container>
-                    );
-                };
+                ))}
+            </div>
+            <Modal
+                show={showModal}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you sure you want to delete?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Deleting a calculator type is permanent and will delete all corresponding data.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="danger" onClick={handleDelete}>Delete</Button>
+                </Modal.Footer>
+            </Modal>
 
-                export default CalculatorTypes;
+        </Container>
+
+    );
+};
+
+export default CalculatorTypes;
