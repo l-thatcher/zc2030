bec58bac43a3848151e4a12c5fec81f05b2989e1
c1847017 <yiallouridess@cardiff.ac.uk>
2022-03-15 21:29:06 +0000

    [66] Feature: Secure Calculator Types, Categories, Inputs and Results API endpoints

--- serverless/pages/api/calculators/[typeId]/index.js
+++ serverless/pages/api/calculators/[typeId]/index.js
@@ -1,5 +1,6 @@
 import {execute_query} from "../../../../utils/db";
 import {deleteCalculatorType} from "../../../../services/PrismaService";
+import {getSession} from "next-auth/react";
 
 const getCalculatorCategoriesByTypeId = `SELECT CalculatorType.id, CalculatorCategory.id, CalculatorCategory.name FROM CalculatorCategory 
                                          JOIN CalculatorType ON CalculatorType.id = CalculatorCategory.type_id 
@@ -16,74 +17,79 @@ const deleteCalculatorCategory = `DELETE FROM CalculatorCategory
                                   WHERE id = ?`;
 
 export default async function handler(req, res) {
+    // const session = await getSession({ req })
     const {typeId} = req.query;
 
-    switch (req.method) {
+    // if (session) {
+        switch (req.method) {
+            // Get data from database
+            case "GET":
+                try {
+                    const result = await execute_query(
+                        getCalculatorCategoriesByTypeId,
+                        typeId
+                    );
+                    res.status(200).json(result);
+                } catch (e) {
+                    res.status(500).json({message: e.message});
+                }
+                break;
 
-        // Get data from database
-        case "GET":
-            try {
-                const result = await execute_query(
-                    getCalculatorCategoriesByTypeId,
-                    typeId
-                );
-                res.status(200).json(result);
-            } catch (e) {
-                res.status(500).json({message: e.message});
-            }
-            break;
+            // Create data from database
+            case "POST":
 
-        // Create data from database
-        case "POST":
+                const typeIdPost = req.body[0];
+                const namePost = req.body[1];
 
-            const typeIdPost = req.body[0];
-            const namePost = req.body[1];
+                try {
+                    const result = await execute_query(
+                        saveCalculatorCategory, [
+                            typeIdPost,
+                            namePost
+                        ]);
+                    res.status(200).json(result);
+                } catch (e) {
+                    res.status(500).json({message: e.message});
+                }
+                break;
 
-            try {
-                const result = await execute_query(
-                    saveCalculatorCategory, [
-                        typeIdPost,
-                        namePost
-                    ]);
-                res.status(200).json(result);
-            } catch (e) {
-                res.status(500).json({message: e.message});
-            }
-            break;
+            // Delete data from database
+            case "DELETE":
+                try {
+                    await deleteCalculatorType(BigInt(typeId))
+                    res.status(204).json();
+                } catch
+                    (e) {
+                    console.log(e)
+                    res.status(500).json({message: e.message});
+                }
 
-        // Delete data from database
-        case "DELETE":
-            try {
-                await deleteCalculatorType(BigInt(typeId))
-                res.status(204).json();
-            } catch
-                (e) {
-                console.log(e)
-                res.status(500).json({message: e.message});
-            }
+                break;
 
-            break;
+            // Update data from database
+            case
+            "PUT":
 
-        // Update data from database
-        case
-        "PUT":
+                const typeIdPut = req.body[0];
+                const namePut = req.body[1];
+                const categoryIdPut = req.body[2];
 
-            const typeIdPut = req.body[0];
-            const namePut = req.body[1];
-            const categoryIdPut = req.body[2];
+                try {
+                    const result = await execute_query(
+                        updateCalculatorCategory, [
+                            typeIdPut,
+                            namePut,
+                            categoryIdPut
+                        ]);
+                    res.status(200).json(result);
+                } catch (e) {
+                    res.status(500).json({message: e.message});
+                }
 
-            try {
-                const result = await execute_query(
-                    updateCalculatorCategory, [
-                        typeIdPut,
-                        namePut,
-                        categoryIdPut
-                    ]);
-                res.status(200).json(result);
-            } catch (e) {
-                res.status(500).json({message: e.message});
-            }
-
-            break;
-    }
+                break;
+        }
+    // } else {
+    //     res.status(401)
+    // }
+    // res.end()
 }
