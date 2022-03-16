54dec62a9975bd689c4f9a42c16d96685f738a06
c1847017 <yiallouridess@cardiff.ac.uk>
2022-03-16 21:04:26 +0000

    [74] Feature: Initialize the project into a different directory

--- serverless/smart-contract/test/ZCTokenTest.js
+++
@@ -1,7 +0,0 @@
-contract('ZCToken', function(accounts) {
-    it("should assert true", function(done) {
-        var ZCToken = ZCToken.deployed();
-        assert.isTrue(true);
-        done();
-    });
-});
