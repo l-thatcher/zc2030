44acc89e6ddb4529b256e7bafdeeb7fea42c5493
Clive Tsungu <ctsungu@gmail.com>
2022-03-18 11:56:36 +0000

    [71] Feature: redirect navbar  Co-authored-by: Tom Rowbotham<#  rowbothamt@cardiff.ac.uk>

--- serverless/Components/LoginSession.js
+++ serverless/Components/LoginSession.js
@@ -20,7 +20,8 @@ function LoginPage() {
           </button>
 
           <button className=" mt-4 -ml-1 flex items-center text-sm font-medium text-white bg-green-400 hover:bg-green-800  font-medium rounded-lg text-sm px-2 py-2.5 text-center mr-2 mb-2 "
-                  onClick={() => signOut()}>
+                  onClick={() => signOut(null,
+                      { callbackUrl: 'http://localhost:3000/registerRedirect' })}>
 
               <span className="ml-1">Sign out</span>
           </button>
