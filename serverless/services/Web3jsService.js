1c82739244e8b6ef08ba3047708d2347265bf6c7
c1919822 <rowbothamt@cardiff.ac.uk>
2022-03-18 00:30:36 +0000

    [71] Feature: Started to setup a maticjs client
    Co-authored-by: Clive Tsungu <tsunguc@cardiff.ac.uk>

--- serverless/services/Web3jsService.js
+++ serverless/services/Web3jsService.js
@@ -5,6 +5,7 @@ const web3 = new Web3() //Insert eth node info
 
 export const createEncryptedWallet = () =>{
     const account = web3.eth.accounts.create()
+    console.log(account)
     return web3.eth.accounts.encrypt(account.privateKey, process.env.ENCRYPTION_PASSWORD)
 }
 
