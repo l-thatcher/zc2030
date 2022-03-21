fceaa9333602b303189b0449471cad75d23ee930
nassirin@cardiff.ac.uk <nassirin@cardiff.ac.uk>
2022-03-21 20:13:47 +0000

    [83] Feature: Adding paypal integration with Sandbox purchase gateway - co-author: Tom Rowbotham [RowbothamT@cardiff.ac.uk]

--- serverless/Components/projectDetails/BuyCard.js
+++ serverless/Components/projectDetails/BuyCard.js
@@ -1,10 +1,11 @@
 import ProgressB from "./ProgressB";
+import {PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js";
 
 function BuyCard(props) {
-  return (
-    <div className="w-96  px-4">
-      <div
-        className="
+    return (
+        <div className="w-96  px-4">
+            <div
+                className="
                bg-white
                rounded-xl
                relative
@@ -21,41 +22,66 @@ function BuyCard(props) {
                bg-white rounded-xl transform
              duration-300 shadow-lg
              hover:shadow-2xl hover:font-bold"
-      >
-        <h2 className="font-bold  mb-1 text-[34px] text-green-600 pb-3 ">
-          £{props.detailsProps.cptgbp}
-          <span className="text-base text-body-color text-black  font-medium">
+            >
+                <h2 className="font-bold  mb-1 text-[34px] text-green-600 pb-3 ">
+                    £{props.detailsProps.cptgbp}
+                    <span className="text-base text-body-color text-black  font-medium">
             / tCo2e
           </span>
-        </h2>
-        <p className="font-semibold text-gray-400">
-          {" "}
-          Total Supply: {props.detailsProps.totalsupply}{" "}
-        </p>
-        <p className="font-semibold text-gray-400">
-          {" "}
-          Available Supply: {props.detailsProps.remainingsupply}{" "}
-        </p>
-        <p className="font-bold">
-          How many tonnes of carbon would you like to buy?
-        </p>
-        <input
-          className="w-full form-control border border-solid border-gray-300 rounded block px-6 py-2.5 mb-3"
-          type="number"
-          placeholder="tonnes"
-        />
-        <button
-          type="button"
-          className="mb-2 w-full inline-block px-6 py-2.5 bg-green-500 text-white font-medium
-                                             text-xs leading-normal uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg
-                                            focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800
-                                              active:shadow-lg  duration-150 ease-in-out"
-        >
-          Buy Carbon
-        </button>
-      </div>
-    </div>
-  );
+                </h2>
+                <p className="font-semibold text-gray-400">
+                    {" "}
+                    Total Supply: {props.detailsProps.totalsupply}{" "}
+                </p>
+                <p className="font-semibold text-gray-400">
+                    {" "}
+                    Available Supply: {props.detailsProps.remainingsupply}{" "}
+                </p>
+                <p className="font-bold">
+                    How many tonnes of carbon would you like to buy?
+                </p>
+                <input
+                    className="w-full form-control border border-solid border-gray-300 rounded block px-6 py-2.5 mb-3"
+                    type="number"
+                    placeholder="tonnes"
+                />
+                {/*<button*/}
+                {/*  type="button"*/}
+                {/*  className="mb-2 w-full inline-block px-6 py-2.5 bg-green-500 text-white font-medium*/}
+                {/*                                     text-xs leading-normal uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg*/}
+                {/*                                    focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800*/}
+                {/*                                      active:shadow-lg  duration-150 ease-in-out"*/}
+                {/*>*/}
+                {/*  Buy Carbon*/}
+                {/*</button>*/}
+
+                {/*Paypal Button*/}
+                <PayPalScriptProvider
+                    options={{"client-id": "AXEtfxC428uLLJR3HGUzT5ZywDHc3XeGG_ZUdXN41yspvFPvl1fLmcvNYHJlgQdLW5L6elWYHIMxQ1RQ"}}>
+                    <PayPalButtons
+                        createOrder={(data, actions) => {
+                            return actions.order.create({
+                                purchase_units: [
+                                    {
+                                        amount: {
+                                            value: "1.99",
+                                        },
+                                    },
+                                ],
+                            });
+                        }}
+                        onApprove={(data, actions) => {
+                            return actions.order.capture().then((details) => {
+                                const name = details.payer.name.given_name;
+                                alert(`Transaction completed by ${name}`);
+                            });
+                        }}
+                    />
+                </PayPalScriptProvider>
+
+            </div>
+        </div>
+    );
 }
 
 export default BuyCard;
