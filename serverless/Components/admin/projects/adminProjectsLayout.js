eb934e915a3b1e1bfb85b366ac9a047e4e17a989
Clive Tsungu <ctsungu@gmail.com>
2022-03-22 13:52:36 +0000

    [70] feature: made View context provider in parent component

--- serverless/Components/admin/projects/adminProjectsLayout.js
+++ serverless/Components/admin/projects/adminProjectsLayout.js
@@ -2,11 +2,16 @@
 import Projects from './Projects'
 import CreateProject from "./CreateProject";
 import ProjectCard from "../../ProjectCard";
+import {ViewContext} from "../../../Contexts/ViewContext";
+import {useState} from "react";
 
 const AdminProjectsLayout = (props) => {
 
     const allProjects = props.projects
 
+    const [view, setView] = useState('current projects');
+    //project to edit
+    const [editPrj, setEditPrj] = useState('current projects');
 
     return(
 
@@ -14,10 +19,9 @@ const AdminProjectsLayout = (props) => {
             <h1 className='font-bold text-3xl px-10 mt-10'>
 
             </h1>
-
-           <Projects projects={allProjects}/>
-
-
+            <ViewContext.Provider value={{view,setView,editPrj,setEditPrj}}>
+                <Projects projects={allProjects}/>
+            </ViewContext.Provider>
         </div>
     )
 
