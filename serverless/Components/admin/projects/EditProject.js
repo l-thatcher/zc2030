f25d05c6692fe68679a571ed0fac0d9f059ff143
Clive Tsungu <ctsungu@gmail.com>
2022-03-25 04:16:24 +0000

    [70] Styling: Removed fields that shouldnt be changed on the edit project page

--- serverless/Components/admin/projects/EditProject.js
+++ serverless/Components/admin/projects/EditProject.js
@@ -143,19 +143,7 @@ const EditProject =(props)=> {
                                 </div>
                             </div>
                             <div className="w-full lg:w-6/12 px-4">
-                                <div className="relative w-full mb-3">
-                                    <label
-                                        className="block capitalize text-blueGray-600 text-xs font-bold mb-2"
-                                        htmlFor="grid-password"
-                                    >
-                                        Total Supply
-                                    </label>
-                                    <input value={totalsupply}
-                                        type="number"
-                                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                         onChange={(e) => setTotalsupply(e.target.value)}
-                                    />
-                                </div>
+
                                 <div className="relative w-full mb-3">
                                     <label
                                         className="block capitalize text-blueGray-600 text-xs font-bold mb-2"
