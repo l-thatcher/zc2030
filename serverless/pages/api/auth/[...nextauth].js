90439ff54ca68e2e3c4eca127d1863cc9f62e75c
Louis <thatcherlg@cardiff.ac.uk>
2022-03-29 02:39:19 +0100

    [84] FEATURE - set up a sign in page
    Co-authored-by: name <ctsungu@gmail.com>

--- serverless/pages/api/auth/[...nextauth].js
+++ serverless/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ import { PrismaClient } from "@prisma/client";
 import { addWalletToUserId } from "../../../services/PrismaService";
 import { createEncryptedWallet } from "../../../services/Web3jsService";
 
+
 dotenv.config();
 
 //https://next-auth.js.org/adapters/prisma
@@ -50,4 +51,7 @@ export default NextAuth({
       await addWalletToUserId(user.id, JSON.stringify(wallet), wallet.address);
     },
   },
+  pages: {
+    signIn: '/auth/signin',
+  }
 });
