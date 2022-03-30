a8ac8a90282944f26905276eed83e5f4623ccba8
Louis <thatcherlg@cardiff.ac.uk>
2022-03-30 02:21:54 +0100

    [84] FIX - seems to work with it as a div. no idea how blank card can cause that but seems to work now at least

--- serverless/Components/admin/projects/BlankCard.js
+++ serverless/Components/admin/projects/BlankCard.js
@@ -28,12 +28,11 @@ const Card = (props) => {
 
                     <div className="mt-1 flex justify-center border-2 border-gray-300 border-dashed rounded-md">
                         <div className="space-y-1 text-center">
-                         {/*   <button*/}
-                         {/*       className=" mt-3 mb-3 mr-5 bg-green-500  justify-center text-white font-bold capitalize text-xs px-4 py-2*/}
-                         {/*rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"*/}
-                         {/*       type="button" >*/}
-                         {/*       Create Project*/}
-                         {/*   </button>*/}
+                            <div className=" mt-3 mb-3 mr-5 bg-green-500  justify-center text-white font-bold capitalize text-xs px-4 py-2
+                         rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                                type="button" >
+                                Create Project
+                            </div>
                             <svg className="mx-auto h-full w-full text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                 <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
