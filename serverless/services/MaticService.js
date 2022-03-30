3d1f359c2bfe9b13e40356718e1cc98d95239b66
Tom Rowbotham <rowbothamt@cardiff.ac.uk>
2022-03-30 21:15:59 +0100

    [83] Feature: WORKING TRANSFER

--- serverless/services/MaticService.js
+++ serverless/services/MaticService.js
@@ -1,58 +1,59 @@
-import { POSClient, use } from "@maticnetwork/maticjs";
-import { Web3ClientPlugin } from "@maticnetwork/maticjs-web3";
-import HDWalletProvider from "@truffle/hdwallet-provider";
-import {decryptWallet} from "./Web3jsService";
-import Web3 from "web3";
-import * as fs from "fs";
-
-// install web3 plugin
-use(Web3ClientPlugin);
-
-//Setting up MaticJS
-const posClient = new POSClient();
-const parentRPC = "https://rpc-mumbai.maticvigil.com/"; //Matic chain
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-posClient.init({
-  network: "testnet",
-  version: "mumbai",
-  parent: {
-    provider: new HDWalletProvider({
-      mnemonic: {
-        phrase:
-        mnemonic
-      },
-      providerOrUrl: parentRPC,
-      pollingInterval: 8000
-    }),
-    defaultConfig: {
-      from: "0xDb6C1E3eE0370Abfc240Df451Ddd72FD05315F4B",
-    },
-  },
-  child: {
-    provider: new HDWalletProvider({
-      mnemonic: {
-        phrase:
-        mnemonic
-      },
-      providerOrUrl: "https://rpc-mumbai.maticvigil.com/",
-      pollingInterval: 8000
-    }),
-    defaultConfig: {
-      from: "0xDb6C1E3eE0370Abfc240Df451Ddd72FD05315F4B",
-    },
-  },
-});
-
-const erc20ParentToken = posClient.erc20(
-  "0000000000000000000000000000000000001010"
-);
-
-export async function getMaticBalance(userAddress) {
-  return (await erc20ParentToken.getBalance(userAddress)) / 1000000000000000000;
-}
-
-//Transfers tokens from one address to another.
-export const transferMatic = async (to, amount) => {
-  return (await erc20ParentToken.transfer(amount, to))
-};
-
+// import { POSClient, use } from "@maticnetwork/maticjs";
+// import { Web3ClientPlugin } from "@maticnetwork/maticjs-web3";
+// import HDWalletProvider from "@truffle/hdwallet-provider";
+// import {decryptWallet} from "./Web3jsService";
+// import Web3 from "web3";
+// import * as fs from "fs";
+//
+// // install web3 plugin
+// use(Web3ClientPlugin);
+//
+// //Setting up MaticJS
+// const posClient = new POSClient();
+// const parentRPC = "https://rpc-mumbai.maticvigil.com/"; //Matic chain
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+// const devWallet = "0x427897086E5Ac14561a7559B694617EaF5e0c1d8"
+// posClient.init({
+//   network: "testnet",
+//   version: "mumbai",
+//   parent: {
+//     provider: new HDWalletProvider({
+//       mnemonic: {
+//         phrase:
+//         mnemonic
+//       },
+//       providerOrUrl: parentRPC,
+//       pollingInterval: 8000
+//     }),
+//     defaultConfig: {
+//       from: devWallet,
+//     },
+//   },
+//   child: {
+//     provider: new HDWalletProvider({
+//       mnemonic: {
+//         phrase:
+//         mnemonic
+//       },
+//       providerOrUrl: "https://rpc-mumbai.maticvigil.com/",
+//       pollingInterval: 8000
+//     }),
+//     defaultConfig: {
+//       from: devWallet,
+//     },
+//   },
+// });
+//
+// const erc20ParentToken = posClient.erc20(
+//   "0000000000000000000000000000000000001010"
+// );
+//
+// export async function getMaticBalance(userAddress) {
+//   return (await erc20ParentToken.getBalance(userAddress)) / 1000000000000000000;
+// }
+//
+// //Transfers tokens from one address to another.
+// export const transferMatic = async (to, amount) => {
+//   return (await erc20ParentToken.transfer(amount, to))
+// };
+//
--- serverless/services/ZCTService.js
+++ serverless/services/ZCTService.js
@@ -5,16 +5,13 @@ import log from "tailwindcss/lib/util/log";
 import * as fs from "fs";
 import {decryptWallet} from "./Web3jsService";
 import {getPrivateKeys} from "@truffle/hdwallet-provider/dist/constructor/getPrivateKeys";
-import {errorParser} from "tedious/lib/token/infoerror-token-parser";
-import {getMaticBalance, transferMatic} from "./MaticService";
 
-const childRPC = "https://polygon-mumbai.infura.io/v3/35b570830e7b48d5952f5d976d7c05a0"
-    // 'https://rpc-mumbai.maticvigil.com/'
+const childRPC = 'https://rpc-mumbai.maticvigil.com/'
 // https://polygon-mumbai.infura.io/v3/579ec05cfce44d31854d6f693d5fa907
 // const childRPC =
 //     "wss://ropsten.infura.io/ws/v3/579ec05cfce44d31854d6f693d5fa907";
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-const devWallet = "0x427897086E5Ac14561a7559B694617EaF5e0c1d8"
+const devWallet="0x427897086E5Ac14561a7559B694617EaF5e0c1d8"
 const web3 = new Web3(
     new HDWalletProvider({
         mnemonic: {
@@ -22,17 +19,13 @@ const web3 = new Web3(
             mnemonic,
         },
         providerOrUrl: childRPC,
-        pollingInterval: 8000,
-        reconnect: {
-            auto: true,
-            delay: 5000,
-        }
+        pollingInterval: 8000
     })
 );
 
 
 const tokenAddress = "0x26B82ef7812D5D2f4Ca3bD8140FC5642702D9e0e";
-web3.eth.transactionPollingTimeout = 5000;
+
 const erc20Contract = new web3.eth.Contract(abiJson().abi, tokenAddress);
 
 export const getZCTBalance = async (address) => {
@@ -51,20 +44,10 @@ export const getZCTBalances = async (addresses) => {
     return addresses
 };
 
-export const gasInFarmWallet = async (from, to) => {
-
-
-}
-
 
 //Transfers tokens from one address to another. Request sent by dev wallet so dev wallet pays gas fees.
 export const transferZCT = async (from, to, amount) => {
     const fromWallet = decryptWallet(from)
-    const fromAddress = fromWallet.address
-    const gasLimit = ("10000000108")
-    console.log("123")
-    const gasEstimate = web3.eth.getGasPrice()
-    console.log("456")
     const localWeb3Connection = new Web3(
         new HDWalletProvider({
             privateKeys: [fromWallet.privateKey],
@@ -72,67 +55,37 @@ export const transferZCT = async (from, to, amount) => {
             pollingInterval: 8000
         })
     );
-    console.log("1111")
     const localErc20Contract = new localWeb3Connection.eth.Contract(abiJson().abi, tokenAddress);
+    //Called from the farm wallet
+    await localErc20Contract.methods
+        .approve(devWallet, Web3.utils.toWei(amount))
+        .send({from: fromWallet.address});
 
-
-    console.log("Matic Balance: " + await getMaticBalance(fromAddress))
-
-    console.log("Matic Balance: " + await getMaticBalance(devWallet))
-
-    const farmBalance = await getMaticBalance(fromAddress)
-    const devBalance = await getMaticBalance(devWallet)
-
-    if (farmBalance <= "0.5") {
-        console.log("Farm does not have enough Gas, attempting to transfer gas")
-        await transferMatic(fromAddress, "0.5")
-        console.log(`Dev balance: ${devBalance}`)
-    }
-    localErc20Contract.methods // Approve Farm to User
-        .approve(fromAddress, Web3.utils.toWei(amount))
-        .send({from: fromAddress}).then
-    {
-        (await localErc20Contract.methods // Transfer Farm to user
-            .transferFrom(fromAddress, to, Web3.utils.toWei(amount))
-            .send({from: fromAddress}));
-    }
-    console.log("Matic Balance: " + await getMaticBalance(fromAddress))
-
-
+    return erc20Contract.methods
+        .transferFrom(fromWallet.address, to, Web3.utils.toWei(amount))
+        .send({from: devWallet})
+        .then((balance) => {
+            return Web3.utils.fromWei(balance.toString());
+        });
 };
-
-// export const gaslessZCT = async (address)
-
-const getSignatureParameters = signTxHash => {
-    console.log("here9090")
-    console.log(signTxHash)
-
-    if (!web3.utils.isHexStrict(signTxHash)) {
-        throw new Error(
-            'Given value "'.concat(signTxHash, '" is not a valid hex string.')
-        );
-    }
-    const r = signTxHash.slice(0, 66);
-    const s = "0x".concat(signTxHash.slice(66, 130));
-    let v = "0x".concat(signTxHash.slice(130, 132));
-    v = web3.utils.hexToNumber(v);
-    if (![27, 28].includes(v)) v += 27;
-    return {
-        r: r,
-        s: s,
-        v: v
-    };
-};
-
 
 export const mintZCT = async (address, amount) => {
     return erc20Contract.methods
         .mint(address, Web3.utils.toWei(amount))
         .send({from: devWallet, value: "0"})
         .then((balance) => {
-            return Web3.utils.fromWei(balance.toString());
+            return Web3.utils.fromWei(balance);
         });
 };
+
+export const fetchTransactionsByAddress = async (address) => {
+
+    const res = await fetch(`https://api-testnet.polygonscan.com/api?module=account&action=txlist&address=0xDb6C1E3eE0370Abfc240Df451Ddd72FD05315F4B&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=DKKBHH9D4HYB15JA4P1YVEZQX316D78JAR`);
+    console.log(await res.json());
+
+
+
+}
 
 
 
